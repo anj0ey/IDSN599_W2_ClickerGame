2e59e001a525c1267813de1d3cd4b09bcc8e200d
anj0ey <cajigal@usc.edu>
2025-09-10 15:06:14 -0700

    Reupload Parent Folder

---
+++ W2D2/W2D2/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  W2D2
+//
+//  Created by Anjo on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isOn = false
+    @State var numClick = 0
+    @State var achievementMessage: String? = nil
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Tap below for free coins")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+            }.padding()
+            
+            VStack(spacing: 20) {
+                Text("Coins: \(numClick)")
+                    .font(.largeTitle)
+            }
+            
+            Button("🪙") {
+                onClick()
+            }.font(.system(size: 200))
+            
+            if let message = achievementMessage {
+                Text(message)
+                    .font(.headline)
+                    .foregroundColor(.green)
+                    .padding()
+                    .multilineTextAlignment(.center)
+            }
+        }.padding()
+    }
+    
+    func onClick() {
+        numClick += 1
+        print(numClick)
+
+        achievementMessage = achievement(numClick: numClick)
+    }
+    
+    func achievement(numClick: Int) -> String? {
+        if numClick % 10 == 0 {
+            return "🎉 Another 10 coins! Great job, keep on clicking!"
+        }
+        
+        return nil
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ W2D2/W2D2/W2D2App.swift
@@ -0,0 +1,17 @@
+//
+//  W2D2App.swift
+//  W2D2
+//
+//  Created by Anjo on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct W2D2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
